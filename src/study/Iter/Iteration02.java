9b743a5a6888c93b3182720af1b6fa54b5dd6f4a
seoungkwon <s2ngkwon2@naver.com>
2025-04-17 16:52:39 +0900

    반복문 (for,while)

---
+++ src/study/Iter/Iteration02.java
@@ -0,0 +1,64 @@
+package study.Iter;
+
+public class Iteration02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 반복문 while
+
+		/*
+
+		 while(조건식){
+		 	실행문
+		 }
+
+		 */
+
+		//i: 1 2 3 4 5
+		for(int i=1; i<=5; i++) {
+			System.out.println("피곤하다");
+		}
+
+		int i=1;	//초기화
+		while(i<=5) { //조건식
+			//i: 1 2 3 4 5
+			System.out.println("졸리다"); //실행문
+
+			i++; //증감식
+
+			// i=i*2;
+			// i=i-50;
+		}
+
+		System.out.println("=============");
+
+		//i:6
+
+		//1 ~ 5 출력
+		i = 1;
+		while(i<=5) {
+			System.out.println(i);
+			i++;
+		}
+
+		i = 0;
+		while(i<5) {	// 4<5
+			i++;		//i:1 2 3 4
+			System.out.println(i);			
+		}
+
+		//i:5
+		
+		//do while
+		i = 6;
+		
+		while(i<5) {
+			System.out.println("i<5");
+		}
+			
+		do {
+			System.out.println("do while i<5");
+		}	while(i<5);	// do while은 while 뒤에 ; 필요함	// while은 false일때 출력 X , do while은 false일때 한번까지 출력 O
+	}
+
+}
