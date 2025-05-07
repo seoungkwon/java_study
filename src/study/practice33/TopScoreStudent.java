bae97136714852b55dd9db536efe96ea9da0a322
seoungkwon <s2ngkwon2@naver.com>
2025-05-07 09:58:42 +0900

    연습32,33,34 클래스

---
+++ src/study/practice33/TopScoreStudent.java
@@ -0,0 +1,10 @@
+package study.practice33;
+
+public class TopScoreStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/study/practice34/SalaryManMain.java
@@ -0,0 +1,19 @@
+package study.practice34;
+
+public class SalaryManMain {
+
+	public static void main(String[] args) {
+//		SalaryMan sm = new SalaryMan();
+//		sm.getAnnualGross();
+//		new SalaryMan().getAnnualGross();
+		
+//		SalaryMan sm = new SalaryMan();
+		
+//		SalaryMan[] smArr = {sm, new SalaryMan()};
+		
+		System.out.println(new SalaryMan().getAnnualGross());
+		System.out.println(new SalaryMan(2000000).getAnnualGross());
+		
+	}
+
+}
