4f13d8a9afafac8f4ebb4b2e50a13f913af6e646
seoungkwon <s2ngkwon2@naver.com>
2025-05-13 09:34:06 +0900

    연습 42클래스 활용,설계

---
+++ src/study/practice/practice42/Cup.java
@@ -0,0 +1,15 @@
+package study.practice.practice42;
+
+public class Cup {
+
+	/*
+	컵
+	(*주방에 여러개가 있다)
+	- 용량
+	- 재질
+	- 색상
+	*/
+	int ml; //용량 210ml;  190ml; 500ml;
+	String material; //컵의 재질 
+	String color; //컵의 색상
+}
