75608e74b47cf4989ac8f2b60374f5c8e0191259
seoungkwon <s2ngkwon2@naver.com>
2025-05-13 09:38:21 +0900

    클래스 상속 강제타입변환

---
+++ src/study/inherit/inherit06/Bus.java
@@ -0,0 +1,11 @@
+package study.inherit.inherit06;
+
+public class Bus extends Vehicle {
+	public void run() {
+		System.out.println("Bus run");
+	}
+	
+	public void start() {
+		System.out.println("운행 시작~");
+	}
+}
