4d79c571a72b537097bd822e5bb12c01bde10329
seoungkwon <s2ngkwon2@naver.com>
2025-06-27 16:05:47 +0900

    예외처리(try/catch/finally,throws,throw) 수정

--- src/study/exp/Exp02.java
+++ src/study/exp/Exp02.java
@@ -3,8 +3,25 @@ package study.exp;
 public class Exp02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] arr = new int[3];
+		
+		try {
+			
+			//NullPointerException
+			//arr = null;
+			//arr[1] = 10;
+			
+			//ArrayIndexOutOfBoundsException
+			arr[10] = 10;
+			
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException 발생함");
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerException 발생함");
+		} catch (Exception e) {
+			System.out.println("Exception 발생함");
+		}
 	}
 
 }
