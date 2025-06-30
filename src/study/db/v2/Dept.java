e1da5a0d63f96d783a6411f0a88123755f4fc0b1
seoungkwon <s2ngkwon2@naver.com>
2025-06-30 12:49:06 +0900

    DAO구조로 변경, INSERT/UPDATE/DELETE 처리

--- src/study/db/v2/Dept.java
+++ src/study/db/v2/Dept.java
@@ -5,13 +5,13 @@ package study.db.v2;
 //DB 테이블에서 읽어온 데이터를 저장하는 역할
 //DTO (Data Transfer Object)
 public class Dept {  //DeptDTO
-	
+
 	int deptno;
 	String dname;
 	String loc;
-	
+
 	public Dept() {}
-	
+
 	public Dept(int deptno, String dname, String loc) {
 		this.deptno = deptno;
 		this.dname = dname;
@@ -41,7 +41,11 @@ public class Dept {  //DeptDTO
 	public void setLoc(String loc) {
 		this.loc = loc;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", loc=" + loc + "]";
+	}
 	
 }
+
